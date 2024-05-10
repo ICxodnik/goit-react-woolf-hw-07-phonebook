61d27cef209cd1b54e52bb037f000589acf54f91
Natalia <icxodnik@gmail.com>
2024-05-10 20:18:51 +0200

    Revert "use 'redux-persist' to save contacts state in localStorage"
    This reverts commit 70967e301a5892ae87808502bd64de3d43b93d38.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,11 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import phoneBookReducer from './phoneBook.slice';
-
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-
-const reducers = combineReducers({
-  phoneBook: phoneBookReducer,
-});
-
-const persistConfig = {
-  key: 'phoneBook',
-  whitelist: ['phoneBook'],
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
+import { configureStore } from '@reduxjs/toolkit'
+import phoneBookReducer from './phoneBook.slice'
 
 export const store = configureStore({
-  reducer: persistedReducer
-});
+  reducer: {
+    phoneBook: phoneBookReducer,
+  },
+})
 
-export const persistor = persistStore(store);
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
