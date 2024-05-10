70967e301a5892ae87808502bd64de3d43b93d38
Natalia <icxodnik@gmail.com>
2024-05-10 14:41:30 +0200

    use 'redux-persist' to save contacts state in localStorage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import './index.css';
-import { store } from './store'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { persistor, store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import phoneBookReducer from './phoneBook.slice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import phoneBookReducer from './phoneBook.slice';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const reducers = combineReducers({
+  phoneBook: phoneBookReducer,
+});
+
+const persistConfig = {
+  key: 'phoneBook',
+  whitelist: ['phoneBook'],
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    phoneBook: phoneBookReducer,
-  },
-})
+  reducer: persistedReducer
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
